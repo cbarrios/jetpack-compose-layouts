85f6f15c75469bdc21aeb33bd258e093038da17d
cbarrios <lalobarrios16@yahoo.com>
2022-05-04 15:02:53 -0400

    master: (6) Working with lists

--- app/src/main/java/com/lalosapps/layouts/MainActivity.kt
+++ app/src/main/java/com/lalosapps/layouts/MainActivity.kt
@@ -3,10 +3,13 @@ package com.lalosapps.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -21,6 +24,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.lalosapps.layouts.ui.theme.LayoutsTheme
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
@@ -43,7 +47,8 @@ fun MyApp() {
         color = MaterialTheme.colors.background
     ) {
         //PhotographerCard()
-        ScaffoldExample()
+        //ScaffoldExample()
+        SimpleList()
     }
 }
 
@@ -102,7 +107,11 @@ fun ScaffoldExample() {
                     IconButton(onClick = {
                         job?.cancel() // this won't queue the snackbars
                         job = scope.launch {
-                            state.snackbarHostState.showSnackbar("Menu")
+                            state.snackbarHostState.showSnackbar(
+                                message = "Menu",
+                                duration = SnackbarDuration.Short,
+                                actionLabel = "Cancel"
+                            )
                         }
                     }) {
                         Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
@@ -137,5 +146,68 @@ private fun ScaffoldContent(modifier: Modifier = Modifier) {
 fun ScaffoldExamplePreview() {
     LayoutsTheme {
         ScaffoldExample()
+    }
+}
+
+@Composable
+fun SimpleList() {
+    val listSize = 100
+    // We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    // listSize - 1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState, modifier = Modifier.fillMaxSize()) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SimpleListPreview() {
+    LayoutsTheme {
+        SimpleList()
     }
 }
